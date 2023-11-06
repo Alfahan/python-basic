db459028a84fa8d1cfe6176656a999a60b16cdea
Alfahan <ali.farhan160@gmail.com>
2023-11-06 10:26:31 +0700

    Confition If Else

---
+++ condition_if.py
@@ -0,0 +1,11 @@
+x = 5
+y = 8
+
+# karena x lebih kecil dari y adalah benar. maka kode di dalam scope stetemen if akan dijalankan
+if(x < y):
+    print("nilai x lebih kecil dari y.") # ini akan dijalankan
+
+# karena x lebih besar dari y adalah salah. maka kode di dalam scope statemen if tidak akan dijalankan.
+if(x > y):
+    print("nilai x lebih besar dari y.") # ini tidak akan dijalankan
+
---
+++ if.py
@@ -0,0 +1,13 @@
+"""
+Note:
+Penulisan kode di dalam blok atau scope pada Python bergantung pada Indentation (spasi di awal baris). Contoh jika kita ingin 
+menulis kode di dalam blok/scope Statemen if, maka penulisanny seperti ini:
+"""
+
+# penulisan yang benar:
+if(True):
+    print("Benar.") # ada spasi di awal baris (lebih menjorok ke dalam)
+
+# penulisan yang salah:
+if(True):
+print("Benar.") # tidak ada spasi di awal baris (sejajar dengan kode di atasnya)
