c7438b28ccbc6442124e08a10909855ed78a7c94
Alfahan <ali.farhan160@gmail.com>
2023-11-06 09:31:10 +0700

    variable

---
+++ variable_boolean.py
@@ -0,0 +1,9 @@
+# contoh penulisan boolean yang benar
+a = True
+b = False
+
+# contoh penulisan boolean yang salah
+# jika dijalankan, pada baris ini akan error
+c = true 
+
+print(c)
---
+++ variable_string.py
@@ -0,0 +1,17 @@
+# menggunakan single quote 
+myFirtsName = 'Mohamad'
+
+# menggunakan double quote
+myLastName = "Ali Farhan"
+
+# penulisan angka sebagai string
+myAge = "25"
+
+# penulisan string multiline
+myHobby = """Hobi saya adalah
+bersepeda dan bermain game.
+"""
+
+# mencetak 2 variable string dengan perintah print
+print(myFirtsName + " " + myLastName)
+print("Umur saya " + myAge)
